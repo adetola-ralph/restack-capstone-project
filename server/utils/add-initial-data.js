9ef11f7dbe57faffc50bade5b069a8e227174443
Oreofeoluwapo Olutola <oreofeoluwapo.olutola@andela.com>
2019-01-14 22:30:49 +0100

    Add mock data to db

---
+++ server/utils/add-initial-data.js
@@ -0,0 +1,129 @@
+import path from 'path';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+if (process.env.NODE_ENV !== 'production' || process.env.NODE_ENV !== 'staging') {
+  dotenv.config({
+    silent: true,
+    debug: true,
+    path: path.join(__dirname, '../../.env'),
+  });
+}
+
+import CategoryItemModel from '../model/Category';
+
+const mockData = [
+  {
+    title: 'installing git',
+    instructions: [
+      {
+        title: 'Install git on macOS with Homebrew',
+        command: 'brew install git',
+      },
+      {
+        title: 'Install git on Debian-based linux',
+        command: 'sudo apt-get install git',
+      },
+      {
+        title: 'Install git on Windows with Chocolatey',
+        command: 'choco install git',
+      },
+    ],
+  },
+  {
+    title: 'configuration',
+    instructions: [
+      {
+        title: 'Sets the name you want attached to your commit transaction',
+        command: 'git config --global user.name [name]',
+      },
+      {
+        title: 'Sets the email you want atached to your commit transactions',
+        command: 'git config --global user.email [email address]',
+      },
+      {
+        title: 'Enables helpful colorization of command line output',
+        command: 'git config --global color.ui auto',
+      },
+    ],
+  },
+  {
+    title: 'create repositories',
+    instructions: [
+      {
+        title: 'Creates a new local repository with the specified name',
+        command: 'git init [project-name]',
+      },
+      {
+        title: 'CDownloads a project and its entire version history',
+        command: 'git clone [url]',
+      },
+    ],
+  },
+  {
+    title: 'make changes',
+    instructions: [
+      {
+        title: 'Lists all new or modified files to be commited',
+        command: 'git status',
+      },
+      {
+        title: 'Shows file differences not yet staged',
+        command: 'git diff',
+      },
+      {
+        title: 'Add the specified file to the staging area',
+        command: 'git add [file]',
+      },
+      {
+        title: 'Shows file differences between staging and the last file version',
+        command: 'git diff --staged',
+      },
+      {
+        title: 'Unstages the file, but preserve its contents',
+        command: 'git reset [file]',
+      },
+      {
+        title: 'Records staged snapshots in version history',
+        command: 'git commit -m [descriptive message]',
+      },
+    ],
+  },
+  {
+    title: 'moving and removing files',
+    instructions: [
+      {
+        title: 'Deletes the file from the working directory and stages the deletion',
+        command: 'git rm [file]',
+      },
+      {
+        title: 'Removes the file from version control but preserves the file locally',
+        command: 'git rm --cached [file]',
+      },
+      {
+        title: 'Renames the file',
+        command: 'git mv [from] [to]',
+      },
+    ],
+  },
+];
+
+export const insertCateoryItems = (_mockData) => {
+  const dbUrl = process.env.MONGODB_URI;
+  mongoose.connect(dbUrl, { useNewUrlParser: true });
+  const db = mongoose.connection;
+
+  db.once('open', () => {
+    console.log('Database connection successful');
+
+    CategoryItemModel.create(_mockData).then(() => {
+      console.log('Finished creating mock data');
+      db.close();
+    }).catch((err) => {
+      console.log(err);
+      db.close();
+    });
+  });
+};
+
+insertCateoryItems(mockData);
